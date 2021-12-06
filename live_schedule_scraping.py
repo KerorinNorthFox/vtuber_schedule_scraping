586576ab1f2c4da0ca61ac317f08dcc6e950339a
m20_KerorinNF <66837061+KerorinNorthFox@users.noreply.github.com>
2021-12-06 14:50:06 +0900

    Update live_schedule_scraping.py

--- live_schedule_scraping.py
+++ live_schedule_scraping.py
@@ -3,7 +3,7 @@ import requests, sys, random, time
 
 URL = 'https://virtual-youtuber.userlocal.jp/schedules'
 
-V_NAME = '博衣こより'
+V_NAME = ['博衣こより', '勇凪エレナ']
 
 class Schedule_Scraping:
     def __init__(self):
@@ -11,13 +11,17 @@ class Schedule_Scraping:
         text = self.analyzing(alz)
         print(type(text))
         print(text)
-        self.find_v(text)
+        v_data = self.find_v(text)
+        print(v_data)
         time.sleep(3)
     
     def parse(self, url):
         response = requests.get(url, headers={'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.114 Safari/537.36'})
         alz = BeautifulSoup(response.text, 'html.parser')
         return alz
+
+    def nubmer_of_a_tags(self, ):
+        pass
 
     def analyzing(self, alz):
         counter_1 = 0
@@ -32,13 +36,15 @@ class Schedule_Scraping:
 
     def find_v(self, text):
         counter_2 = 0
+        v_data = []
         for v_name in text:
             counter_2 += 1
-            if v_name == V_NAME:
-                print(f">>{v_name}発見、{counter_2}番目")
-                break
-            else:
-                pass
+            for needed_v_name in V_NAME:
+                if v_name == needed_v_name:
+                    v_data.append([v_name, counter_2])
+                else:
+                    pass
+        return v_data
 
 ###START#######################################
 if __name__ == "__main__":
